3b29c811470db2b9892d9bc7e8f23c64acac7735
user.name404 <user.name404@yandex.ru>
2021-10-27 13:08:49 +0300

    Добавлена настройка адреса сервера и порта через переменные окружения

--- src/main.go
+++ src/main.go
@@ -3,12 +3,22 @@ package main
 import (
 	"banki/routes"
 	"net/http"
+	"os"
 )
+
+func getServerString() string {
+	ip:=os.Getenv("SERVER_IP")
+	port:=os.Getenv("SERVER_PORT")
+	if(port=="" || ip==""){
+		panic("Incorrect server settings. Please, set environment variables: SERVER_IP, SERVER_PORT")
+	}
+	return ip+":"+port
+}
 
 func main() {
 	http.HandleFunc("/api/sample/", routes.SampleHandler)
 	http.HandleFunc("/api/export/", routes.ExportHandler)
 	http.HandleFunc("/api/stats/", routes.StatsHandler)
 
-	http.ListenAndServe(":8000", nil)
+	http.ListenAndServe(getServerString(), nil)
 }
